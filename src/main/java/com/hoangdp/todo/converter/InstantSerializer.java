730bc5b0a8cbcfc2574a1d3a414b0699dd9ad57e
hoangdp <kunboy1608@gmail.com>
2023-08-05 16:42:07 +0700

    Move Dataset Initializer

--- src/main/java/com/hoangdp/todo/converter/InstantSerializer.java
+++ src/main/java/com/hoangdp/todo/converter/InstantSerializer.java
@@ -2,21 +2,17 @@ package com.hoangdp.todo.converter;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-public class InstantSerializer extends JsonSerializer<Instant>{
+import com.hoangdp.todo.utils.TimeUtils;
 
-    private static final String PATTERN_FORMAT = "yyyy-MM-dd HH:mm:ss";
+public class InstantSerializer extends JsonSerializer<Instant> {
 
     @Override
     public void serialize(Instant value, JsonGenerator gen, SerializerProvider serializers) throws IOException {        
-        final String str = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone(ZoneOffset.of("+07:00")).format(value);
-        gen.writeString(str);
+        gen.writeString(TimeUtils.instantToString(value));
     }
-    
+
 }
