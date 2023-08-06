bf6e26bfb83353054ef8cb7d462ad0f2975fc769
hoangdp <kunboy1608@gmail.com>
2023-08-06 21:16:10 +0700

    enable Async

---
+++ src/main/java/com/hoangdp/todo/config/AsyncConfig.java
@@ -0,0 +1,18 @@
+package com.hoangdp.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+    /**
+     * Tạo ra Executor cho Async
+     * @return
+     */
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
